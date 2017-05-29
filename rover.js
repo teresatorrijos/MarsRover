a22ead4e3d8b7b5d925b00ba072f98aff3b422e2
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-05-29 20:36:26 +0200

    WIP

--- rover.js
+++ rover.js
@@ -1,25 +1,79 @@
 var myRover = {
-  position: [0,0], 
+  position: [0,0],
   direction: 'N'
 };
 
 function goForward(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
       rover.position[1]++
       break;
+    case 'E':
+      rover.position[0]++
+      break;
     case 'S':
-      rover.position[0]--
+      rover.position[1]--
       break;
     case 'W':
-      rover.position[1]--
+      rover.position[0]--
       break;
   };
 
   console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
 }
 
+function goBack(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[1]--
+      break;
+    case 'E':
+      rover.position[0]--
+      break;
+    case 'S':
+      rover.position[1]++
+      break;
+    case 'W':
+      rover.position[0]++
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+function turnLeft(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W'
+      break;
+    case 'E':
+      rover.direction = 'N'
+      break;
+    case 'S':
+      rover.direction = 'E'
+      break;
+    case 'W':
+      rover.direction = 'S'
+      break;
+  }}
+
+function turnRight(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'E'
+      break;
+    case 'E':
+      rover.direction = 'S'
+      break;
+    case 'S':
+      rover.direction = 'W'
+      break;
+    case 'W':
+      rover.direction = 'N'
+      break;
+  }}
+
+
 goForward(myRover);
+turnLeft(myRover);
+goForward(myRover)
