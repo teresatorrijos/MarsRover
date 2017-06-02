233fc57f319a7b2f2150ddda30549145ecda4fd0
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-06-02 21:02:29 +0200

    without Bonus

--- rover.js
+++ rover.js
@@ -3,77 +3,113 @@ var myRover = {
   direction: 'N'
 };
 
+var grid = new Array(10);
+for (i = 0; i < 10; i++) {
+	grid[i] = new Array(10);
+	for (j = 0; j < 10; j++) {
+		grid[i][j] = "["+i+","+j+"]";
+		}
+}
+
 function goForward(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[1]++
+      rover.position[1]++;
+       if(rover.position[1]>9) rover.position[1] = 0;
       break;
     case 'E':
-      rover.position[0]++
+      rover.position[0]++;
+        if(rover.position[0]>9) rover.position[0] = 0;
       break;
     case 'S':
-      rover.position[1]--
+      rover.position[1]--;
+        if(rover.position[1]<0) rover.position[1] = 9;
       break;
     case 'W':
-      rover.position[0]--
+      rover.position[0]--;
+      if(rover.position[0]<0) rover.position[0] = 9;
       break;
-  };
+     }
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
 }
 
 function goBack(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[1]--
+      rover.position[1]--;
+        if(rover.position[1]<0) rover.position[1] = 9;
       break;
     case 'E':
-      rover.position[0]--
+      rover.position[0]--;
+      if(rover.position[0]<0) rover.position[0] = 9;
       break;
     case 'S':
-      rover.position[1]++
+      rover.position[1]++;
+      if(rover.position[1]>9) rover.position[1] = 0;
       break;
     case 'W':
-      rover.position[0]++
+      rover.position[0]++;
+      if(rover.position[0]>9) rover.position[0] = 0;
       break;
-  };
+  }
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
 }
 
 function turnLeft(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.direction = 'W'
+      rover.direction = 'W';
       break;
     case 'E':
-      rover.direction = 'N'
+      rover.direction = 'N';
       break;
     case 'S':
-      rover.direction = 'E'
+      rover.direction = 'E';
       break;
     case 'W':
-      rover.direction = 'S'
+      rover.direction = 'S';
       break;
   }}
 
 function turnRight(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.direction = 'E'
+      rover.direction = 'E';
       break;
     case 'E':
-      rover.direction = 'S'
+      rover.direction = 'S';
       break;
     case 'S':
-      rover.direction = 'W'
+      rover.direction = 'W';
       break;
     case 'W':
-      rover.direction = 'N'
+      rover.direction = 'N';
       break;
   }}
 
+function marsRover(rover, orders){
+  for(var i = 0; i < orders.length; i++){
+    var orderCode = orders[i];
+    switch(orderCode){
+      case 'f':
+        goForward(rover);
+      break;
+      case 'b':
+        goBack(rover);
+      break;
+      case 'r':
+        turnRight(rover);
+      break;
+      case 'l':
+        turnLeft(rover);
+      break;
+      default : console.log('Invalid order');
+      break;
+    }
+  }
+}
 
-goForward(myRover);
-turnLeft(myRover);
-goForward(myRover)
+var actualOrder = "fflfrbb";
+marsRover(myRover, actualOrder);
