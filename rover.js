1faf3aae401f2386db9a07e7cf68a0a1717bb50f
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-06-05 20:33:40 +0200

    done

--- rover.js
+++ rover.js
@@ -1,60 +1,97 @@
-var myRover = {
+//rovers
+var roverOne = {
   position: [0,0],
-  direction: 'N'
+  direction: 'N',
+  name: "Rover One"
 };
 
+var roverTwo = {
+  position: [3,3],
+  direction: 'N',
+  name: "Rover Two"
+};
+
+//grid
 var grid = new Array(10);
 for (i = 0; i < 10; i++) {
 	grid[i] = new Array(10);
 	for (j = 0; j < 10; j++) {
-		grid[i][j] = "["+i+","+j+"]";
+		grid[i][j] = 0;
 		}
 }
 
+//coloca obstáculos
+function randomPosition(){
+  var x, y;
+  x = Math.floor(Math.random() * 10);
+  y = Math.floor(Math.random() * 10);
+  return [x,y];
+}
+function obstacles (totalObstacles) {
+  for (i=0; i<totalObstacles; i++) {
+    console.log("New obstacle at [" + randomPosition() + "]");
+    var actualObstacles = randomPosition();
+    var x = actualObstacles[0];
+    var y = actualObstacles[1];
+    grid [x][y] = 1;
+  }
+}
+
+//funciones de movimiento
 function goForward(rover) {
+  var potentialPosition = rover.position;
   switch(rover.direction) {
     case 'N':
-      rover.position[1]++;
-       if(rover.position[1]>9) rover.position[1] = 0;
+      potentialPosition[1]++;
+       if(potentialPosition[1]>9) potentialPosition[1] = 0;
       break;
     case 'E':
-      rover.position[0]++;
-        if(rover.position[0]>9) rover.position[0] = 0;
+      potentialPosition[0]++;
+        if(potentialPosition[0]>9) potentialPosition[0] = 0;
       break;
     case 'S':
-      rover.position[1]--;
-        if(rover.position[1]<0) rover.position[1] = 9;
+      potentialPosition[1]--;
+        if(potentialPosition[1]<0) potentialPosition[1] = 9;
       break;
     case 'W':
-      rover.position[0]--;
-      if(rover.position[0]<0) rover.position[0] = 9;
+      potentialPosition[0]--;
+      if(potentialPosition[0]<0) potentialPosition[0] = 9;
       break;
      }
-
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+  if (grid [potentialPosition[0]] [potentialPosition[1]] === 0) {
+    rover.position = potentialPosition;
+  } else {
+  console.log("The rover can't move because there is an obstacle or another rover");
+  }
+  console.log(rover.name + " position: [" + rover.position[0] + ", " + rover.position[1] + "]");
 }
 
 function goBack(rover) {
+  var potentialPosition = rover.position;
   switch(rover.direction) {
     case 'N':
-      rover.position[1]--;
-        if(rover.position[1]<0) rover.position[1] = 9;
+      potentialPosition[1]--;
+        if(potentialPosition[1]<0) potentialPosition[1] = 9;
       break;
     case 'E':
-      rover.position[0]--;
-      if(rover.position[0]<0) rover.position[0] = 9;
+      potentialPosition[0]--;
+      if(potentialPosition[0]<0) potentialPosition[0] = 9;
       break;
     case 'S':
-      rover.position[1]++;
-      if(rover.position[1]>9) rover.position[1] = 0;
+      potentialPosition[1]++;
+      if(potentialPosition[1]>9) potentialPosition[1] = 0;
       break;
     case 'W':
-      rover.position[0]++;
-      if(rover.position[0]>9) rover.position[0] = 0;
+      potentialPosition[0]++;
+      if(potentialPosition[0]>9) potentialPosition[0] = 0;
       break;
   }
-
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+  if (grid [potentialPosition[0]] [potentialPosition[1]] === 0) {
+    rover.position = potentialPosition;
+  } else {
+  console.log("The rover can't move because there is an obstacle or another rover");
+  }
+  console.log(rover.name + " position: [" + rover.position[0] + ", " + rover.position[1] + "]");
 }
 
 function turnLeft(rover) {
@@ -90,6 +127,7 @@ function turnRight(rover) {
   }}
 
 function marsRover(rover, orders){
+  grid [rover.position[0]][rover.position[1]] = 0;
   for(var i = 0; i < orders.length; i++){
     var orderCode = orders[i];
     switch(orderCode){
@@ -109,7 +147,11 @@ function marsRover(rover, orders){
       break;
     }
   }
+  grid [rover.position[0]][rover.position[1]] = 1;
 }
 
-var actualOrder = "fflfrbb";
-marsRover(myRover, actualOrder);
+//¡a probar!
+obstacles (20);
+var actualOrder = "fflfrbblfffrbbbbbbbb";
+marsRover(roverTwo, actualOrder);
+marsRover(roverOne, actualOrder);
